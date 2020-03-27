46dca3c60ae1fa841cc9eb46a023034e3ecfd497
LISA_COOK <lisabpink@gmail.com>
2020-03-27 13:48:05 -0400

    LDC login endpoint test: 2 passing, add register test

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,31 +1,65 @@
-const request = require('supertest');
+const request = require("supertest");
+const server = require("../api/server.js");
+const db = require("../database/dbConfig.js");
 
-const server = require('../api/server.js');
+describe("auth endpoints", function() {
+  describe("POST /api/auth/register", function() {
+    beforeEach(async () => {
+      await db("users").truncate();
+    });
+    
+    it("should return 200 OK", async function() {
+      return await request(server)
+        .post("/api/auth/register")
+        .send({ username: "Cookie Monster", password: "1234" })
+        .set("Accept", "application/json")
+        .expect(201);
+    });
 
-const db = require('../database/dbConfig.js');
+    it("should return json", async function() {
+      return await request(server)
+        .post("/api/auth/register")
+        .send({ username: "Cookie Monster2", password: "1234" })
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/i);
+    });
+  });
 
-describe('auth endpoints', function() {
+  describe("POST /api/auth/login", function() {
+      
+    it("should return 200 OK", async function() {
+      return await request(server)
+        .post("/api/auth/login")
+        .send({ username: "Cookie Monster2", password: "1234" })
+        .set("Accept", "application/json")
+        .expect(200);
+    });
 
-    describe('POST /api/auth/register', function() {
-        beforeEach(async () => {
-            await db('users').truncate();
-        })
+    it("should test user logging in", async () => {});
+  });
+  
+describe("should test registring and loggin in", () => {
+    it("should register users", async () => {
+      const newUser = { username: "Pie Monster", password: "1234" };
+      const res = await request(server)
+        .post("/api/auth/register")
+        .send(newUser);
+      expect(res.status).toBe(201);
+    });
+  
+    it("should test the login function", async () => {
+      const credential = { username: "Pie Monster", password: "1234" };
+      const res = await request(server)
+        .post("/api/auth/login")
+        .send(credential);
+  
+      expect(res.status).toBe(200);
+    });
+  });
+});
 
-        it('should return 200 OK', async function() {
 
-            return await request(server)
-            .post('/api/auth/register')
-            .send({ username: "Len", password: "password" })
-            .set('Accept', 'application/json')
-            .expect(201)
-        })
 
-        it('should return json', async function() {
-            return await request(server)
-            .post('/api/auth/register')
-            .send({ username: "Len", password: "password" })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/i)
-        })
-    })
-})
+// const hasToken = res => {
+//   res.body.should.have.property("token");
+// };
